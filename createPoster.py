44c4bc14a2dbd57fd73060ee9f81ebeec1a5ca79
MARCOS SPINOLA DE SOUZA <110204323+gamertopdelinha@users.noreply.github.com>
2023-11-28 21:23:59 -0300

    YUPIIIIIIIIIIIIIIIIIIIIIII

--- createPoster.py
+++ createPoster.py
@@ -8,3 +8,5 @@ img[0:240, 500:600] = rocket
 text = "Imagem1"
 
 cv2.putText(img,text,(20,220),fontFace = cv2.font,fontScale = 0.6,color = (130,110,45))
+cv2.imshow("Imagem legal", img)
+cv2.waitKey(0)
